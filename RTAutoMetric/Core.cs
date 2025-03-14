bd6689f7dd3f37e0de62b99aced6cd2322d57e23
chimingkuei <chimingkuei@gmail.com>
2025-03-15 03:05:58 +0800

    RTAutoMetric

---
+++ RTAutoMetric/Core.cs
@@ -0,0 +1,211 @@
+﻿using OpenCvSharp.Extensions;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using System.Net.NetworkInformation;
+using Tesseract;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Input;
+using Newtonsoft.Json;
+
+
+namespace RTAutoMetric
+{
+    class Core
+    {
+        TesseractEngine ocr;
+
+        private BitmapImage ConvertBitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Png);
+                memory.Position = 0;
+                BitmapImage bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.StreamSource = memory;
+                bitmapImage.EndInit();
+                bitmapImage.Freeze();
+                return bitmapImage;
+            }
+        }
+
+        public T CaptureRegion<T>(int x, int y, int width, int height, bool show_cursor = false) where T : class
+        {
+            using (Bitmap bitmap = new Bitmap(width, height))
+            {
+                using (Graphics g = Graphics.FromImage(bitmap))
+                {
+                    g.CopyFromScreen(x, y, 0, 0, new System.Drawing.Size(width, height));
+                    if (show_cursor)
+                    {
+                        System.Drawing.Point cursorPosition = System.Windows.Forms.Cursor.Position;
+                        System.Windows.Forms.Cursor cursor = System.Windows.Forms.Cursors.Default;
+                        System.Drawing.Rectangle cursorBounds = new System.Drawing.Rectangle(cursorPosition, cursor.Size);
+                        cursor.Draw(g, cursorBounds);
+                    }
+                }
+                switch (typeof(T))
+                {
+                    case Type t when t == typeof(BitmapImage):
+                        return ConvertBitmapToBitmapImage(bitmap) as T;
+                    case Type t when t == typeof(Mat):
+                        return BitmapConverter.ToMat(bitmap) as T;
+                    case Type t when t == typeof(Bitmap):
+                        return bitmap as T;
+                    default:
+                        throw new NotSupportedException($"Type {typeof(T)} is not supported.");
+                }
+            }
+        }
+
+        public void OCR<T>(T src, string language, out string result)
+        {
+            result = null;
+            ocr = new TesseractEngine("./tessdata", language, EngineMode.Default);
+            Tesseract.Page page = null;
+            switch (src)
+            {
+                case Bitmap bitmap:
+                    page = ocr.Process(bitmap);
+                    break;
+
+                case string filePath when File.Exists(filePath):
+                    page = ocr.Process(Pix.LoadFromFile(filePath));
+                    break;
+            }
+            if (page != null)
+            {
+                result = page.GetText();
+                page.Dispose();
+            }
+        }
+
+
+    }
+
+    class MouseActions
+    {
+        public Polyline maskPath;
+        public bool isDrawing = false;
+        public List<List<System.Windows.Point>> maskPaths = new List<List<System.Windows.Point>>();
+        public Canvas canves = new Canvas();
+        public System.Windows.Media.Color color = System.Windows.Media.Color.FromArgb(120, 0, 0, 0);
+        public double thickness = 10;
+
+        public Polyline PolylineAppearance()
+        {
+            return new Polyline
+            {
+                Stroke = new SolidColorBrush(color),
+                StrokeThickness = thickness,
+                StrokeLineJoin = PenLineJoin.Round,
+                StrokeStartLineCap = PenLineCap.Round,
+                StrokeEndLineCap = PenLineCap.Round,
+            };
+        }
+
+        public void MaskDown(MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                isDrawing = true;
+                maskPath = PolylineAppearance();
+                canves.Children.Add(maskPath);
+            }
+        }
+
+        public void MaskMove(MouseEventArgs e)
+        {
+            if (isDrawing && maskPath != null)
+            {
+                System.Windows.Point currentPoint = e.GetPosition(canves);
+                maskPath.Points.Add(currentPoint);
+            }
+        }
+
+        public void MaskUp()
+        {
+            if (isDrawing && maskPath != null)
+            {
+                maskPaths.Add(maskPath.Points.ToList()); // 儲存遮罩點座標
+                List<System.Windows.Point> pointsList = maskPath.Points.ToList();
+                // 列印點座標
+                Console.WriteLine("Mask Path Points:");
+                foreach (var point in pointsList)
+                {
+                    Console.WriteLine($"({point.X}, {point.Y})");
+                }
+            }
+            isDrawing = false;
+        }
+
+        public void MaskMouseRightButtonDown()
+        {
+            var maskElements = canves.Children.OfType<Polyline>().ToList();
+            foreach (var element in maskElements)
+            {
+                canves.Children.Remove(element);
+            }
+        }
+
+        private void SaveMaskToFile(string filePath)
+        {
+            var serializableData = maskPaths
+                .Select(path => path.Select(p => Tuple.Create(p.X, p.Y)).ToList())
+                .ToList();
+            string json = JsonConvert.SerializeObject(serializableData, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        private void LoadMaskFromFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                var serializableData = JsonConvert.DeserializeObject<List<List<Tuple<double, double>>>>(json);
+                maskPaths = serializableData
+                    .Select(path => path.Select(t => new System.Windows.Point(t.Item1, t.Item2)).ToList())
+                    .ToList();
+                RedrawMasks();
+            }
+        }
+
+        private void RedrawMasks()
+        {
+            canves.Children.Clear();
+            foreach (var points in maskPaths)
+            {
+                Polyline polyline = PolylineAppearance();
+                canves.Children.Add(polyline);
+            }
+        }
+
+        private void SaveCanvasToImage(string filePath)
+        {
+            RenderTargetBitmap renderBitmap = new RenderTargetBitmap(
+                (int)canves.ActualWidth, (int)canves.ActualHeight,
+                96d, 96d, PixelFormats.Pbgra32);
+            renderBitmap.Render(canves);
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(renderBitmap));
+                encoder.Save(fileStream);
+            }
+            Console.WriteLine("影像已儲存：" + filePath);
+        }
+
+
+    }
+
+}
---
+++ RTAutoMetric/MainWindow.xaml.cs
@@ -0,0 +1,281 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using static RTAutoMetric.BaseLogRecord;
+using System.Windows.Media.Media3D;
+using System.Drawing;
+using System.Text.RegularExpressions;
+using System.Windows.Controls.Primitives;
+using netDxf.Entities;
+
+
+namespace RTAutoMetric
+{
+    #region Config Class
+    public class SerialNumber
+    {
+        [JsonProperty("Parameter1_val")]
+        public string Parameter1_val { get; set; }
+        [JsonProperty("Parameter2_val")]
+        public string Parameter2_val { get; set; }
+    }
+
+    public class Model
+    {
+        [JsonProperty("SerialNumbers")]
+        public SerialNumber SerialNumbers { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("Models")]
+        public List<Model> Models { get; set; }
+    }
+    #endregion
+
+    public partial class MainWindow : System.Windows.Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region Config
+        private SerialNumber SerialNumberClass()
+        {
+            SerialNumber serialnumber_ = new SerialNumber
+            {
+                Parameter1_val = Parameter1.Text,
+                Parameter2_val = Parameter2.Text
+            };
+            return serialnumber_;
+        }
+
+        private void LoadConfig(int model, int serialnumber, bool encryption = false)
+        {
+            List<RootObject> Parameter_info = Config.Load(encryption);
+            if (Parameter_info != null)
+            {
+                Parameter1.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter1_val;
+                Parameter2.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter2_val;
+            }
+            else
+            {
+                // 結構:2個Models、Models下在各2個SerialNumbers
+                SerialNumber serialnumber_ = SerialNumberClass();
+                List<Model> models = new List<Model>
+                {
+                    new Model { SerialNumbers = serialnumber_ },
+                    new Model { SerialNumbers = serialnumber_ }
+                };
+                List<RootObject> rootObjects = new List<RootObject>
+                {
+                    new RootObject { Models = models },
+                    new RootObject { Models = models }
+                };
+                Config.SaveInit(rootObjects, encryption);
+            }
+        }
+       
+        private void SaveConfig(int model, int serialnumber, bool encryption = false)
+        {
+            Config.Save(model, serialnumber, SerialNumberClass(), encryption);
+        }
+        #endregion
+
+        #region Dispatcher Invoke 
+        public string DispatcherGetValue(TextBox control)
+        {
+            string content = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                content = control.Text;
+            });
+            return content;
+        }
+
+        public void DispatcherSetValue(string content, TextBox control)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                control.Text = content;
+            });
+        }
+        #endregion
+
+        #region ROI Selection Operation
+        //private void ShowRect(System.Windows.Point point)
+        //{
+        //    var rect = new System.Windows.Rect(_startPoint, point);
+        //    Rectangle.Margin = new Thickness(rect.Left, rect.Top, 0, 0);
+        //    Rectangle.Width = rect.Width;
+        //    Rectangle.Height = rect.Height;
+        //}
+
+        //private void DrawROI_MouseDown(object sender, MouseButtonEventArgs e)
+        //{
+        //    _started = true;
+        //    _startPoint = e.GetPosition(Display_Screen);
+        //    //Console.WriteLine($"X座標:{e.GetPosition(Display_Screen).X}");
+        //    //Console.WriteLine($"Y座標:{e.GetPosition(Display_Screen).Y}");
+        //}
+
+        //private void DrawROI_MouseUp(object sender, MouseButtonEventArgs e)
+        //{
+        //    _started = false;
+        //}
+
+        //private void DrawROI_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (e.LeftButton == MouseButtonState.Pressed)
+        //    {
+        //        if (_started)
+        //        {
+        //            _endPoint = e.GetPosition(Display_Screen);
+        //            ShowRect(_endPoint);
+        //        }
+        //    }
+        //}
+
+        //private System.Windows.Point ConvertCoord(System.Windows.Point _startPoint)
+        //{
+        //    return new System.Windows.Point(_startPoint.X * 1920 / Display_Screen.ActualWidth, _startPoint.Y * 1080 / Display_Screen.ActualHeight);
+        //}
+        #endregion
+
+        #region Draw Mask
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ma.MaskDown(e);
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            ma.MaskMove(e);
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ma.MaskUp();
+        }
+
+        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ma.MaskMouseRightButtonDown();
+        }
+        #endregion
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig(0, 0);
+            ma.canves = myCanvas;
+        }
+        BaseConfig<RootObject> Config = new BaseConfig<RootObject>();
+        Core Do = new Core();
+        private bool _started;
+        private System.Windows.Point _startPoint;
+        private System.Windows.Point _endPoint;
+        CancellationTokenSource cts;
+        MouseActions ma = new MouseActions();
+        #region Log
+        BaseLogRecord Logger = new BaseLogRecord();
+        //Logger.WriteLog("儲存參數!", LogLevel.General, richTextBoxGeneral);
+        #endregion
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as Button).Name)
+            {
+                case nameof(Capture_Screen):
+                    {
+                        BitmapImage regionImage = Do.CaptureRegion<BitmapImage>(0, 0, 1920, 1080);
+                        Display_Screen.Source = regionImage;
+                        break;
+                    }
+            }
+        }
+
+        private void Workflow_CheckedUnchecked(object sender, RoutedEventArgs e)
+        {
+            var toggleButton = sender as ToggleButton;
+            if (toggleButton.IsChecked == true)
+            {
+                cts = new CancellationTokenSource();
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        if (cts.Token.IsCancellationRequested)
+                        {
+                            Console.WriteLine("thread stop");
+                            return;
+                        }
+                        this.Dispatcher.Invoke(() =>
+                        {
+                            string result;
+                            Do.OCR("TestImage.jpg", "chi_tra", out result);
+                            Match match = Regex.Match(result, @"=\s*(-?\d+(\.\d+)?)");
+                            if (match.Success)
+                            {
+                                string number = match.Groups[1].Value;
+                                Console.WriteLine($"提取的數字: {number}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("未找到數字");
+                            }
+                            GC.Collect();
+                        });
+                        Thread.Sleep(100);
+                    }
+                }, cts.Token);
+            }
+            else
+            {
+                cts.Cancel();
+            }
+        }
+        #endregion
+
+
+    }
+}
